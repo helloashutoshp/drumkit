e23664cd7368430c5f92e7b39657a509d618142f
helloashutoshp <124667480+helloashutoshp@users.noreply.github.com>
2023-05-03 17:10:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ function letter(eve){
         switch(io){
         
             case "w":
-                var ty=new Audio("sounds/crash.mp3");
+                var ty=new Audio("crash.mp3");
                 ty.play();
                 break;
                 case "a":
-                    var ty1=new Audio("sounds/kick-bass.mp3");
+                    var ty1=new Audio("kick-bass.mp3");
                     ty1.play();
                     break;
                     case "s":
-                        var ty2=new Audio("sounds/snare.mp3");
+                        var ty2=new Audio("snare.mp3");
                         ty2.play();
                         break;
                         case "d":
-                            var ty3=new Audio("sounds/tom-1.mp3");
+                            var ty3=new Audio("tom-1.mp3");
                             ty3.play();
                             break;
                             case "j":
-                                var ty4=new Audio("sounds/tom-2.mp3");
+                                var ty4=new Audio("tom-2.mp3");
                                 ty4.play();
                                 break;
                                 case "k":
-                                    var ty5=new Audio("sounds/tom-3.mp3");
+                                    var ty5=new Audio("tom-3.mp3");
                                     ty5.play();
                                     break;
                                     case "l":
-                                        var ty6=new Audio("sounds/tom-4.mp3");
+                                        var ty6=new Audio("tom-4.mp3");
                                         ty6.play();
                                         break;
     
